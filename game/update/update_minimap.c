5188da285a9eaf5517060eb26154e0ff3de295b0
Timur Mazitov <tmazitov@lab1r5s5.42abudhabi.ae>
2024-08-16 15:03:36 +0400

    update minimap view tostate where player in center of minimap

--- game/update/update_minimap.c
+++ game/update/update_minimap.c
@@ -6,7 +6,7 @@
 /*   By: tmazitov <tmazitov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 16:30:59 by tmazitov          #+#    #+#             */
-/*   Updated: 2024/07/11 23:16:06 by tmazitov         ###   ########.fr       */
+/*   Updated: 2024/08/16 14:49:26 by tmazitov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ void	update_minimap(t_game *game)
 {
 	t_point	player_position;
 	double	player_rotation;
-	t_vector	*player_move;
 
 	player_position.x = game->scene->player->pos->x;
 	player_position.y = game->scene->player->pos->y;
 	player_rotation = game->scene->player->rotation;
-	player_move = game->scene->player->move_vector;
-	minimap_update(game->scene->minimap, player_rotation, player_position, player_move);
+	minimap_update(game->scene->minimap, player_rotation, player_position);
 }
